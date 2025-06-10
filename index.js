feec0a11bf3331a8b53c6be9662a9bb9432f1d6a
hieu <hieuunguyenn63@gmail.com>
2025-06-10 18:34:48 +0200

    calculating avg weight

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const characterRouter = require('./routes/characters')
 const app = express();
 const port = 3000;
 
-app.use('characters', characterRouter)
+app.use('/characters', characterRouter)
 
 app.listen(port, () =>{
     console.log(`[SERVER] is running on port ${port}`)
--- service/characterService.js
+++ service/characterService.js
@@ -1,4 +1,5 @@
 const pool = require('../config/db');
+const dayjs = require('dayjs')
 
 async function fetchCharacters() {
     const characterList = await pool.query('SELECT * FROM character');
@@ -7,20 +8,29 @@ async function fetchCharacters() {
     const characters_count = characters.length;
     let totalAge = 0;
     let ageCount = 0;
+    let totalWeight = 0;
+    let weightCount = 0;
 
     const today = dayjs();
 
     for(const character of characters) {
 
-        const wasBorn = dayjs(character.wasBorn);
-        const age = today.diff(wasBorn, 'year');
+        const born = dayjs(character.born);
+        const age = today.diff(born, 'year');
         totalAge  += age;
-        ageCount +=1;
+        ageCount += 1;
+
+        const weight = Number(character.weight);
+        if (!isNaN(weight)) {
+            totalWeight += weight;
+            weightCount += 1;
+        }
     }
 
     return {
         characters_count,
-        avarage_age: Math.round(totalAge / ageCount)
+        avarage_age: ageCount > 0 ? Math.round(totalAge / ageCount) : null,
+        average_weight: weightCount > 0 ? Math.round(totalWeight / weightCount) : null
     }
 }
 
